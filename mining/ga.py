ff209d831ea8576ca3a561cb48359fd93d19e3c2
Yonatan Oren <yonatano@gmail.com>
2015-07-05 17:58:45 -0700

    small fix in ga

--- mining/ga.py
+++ mining/ga.py
@@ -6,11 +6,18 @@ initial_pool_ = [(0.05, 0.1, 0.1),
                 (0.048, 0.2, 1.0),
                 (0.049, 1.0, 0.3)]
 
-def run_ga(generations=10, organisms_per_generation=10, cutoff=5, initial_pool=initial_pool_, fitness=lambda o:o+1, max_=False):
+initial_pool_2 = [(0.005956095865679523, 0.8221104204538621, 0.007352881885527851), 
+                (0.006145872038018127, 0.9009544044452957, 0.00858503010435533), 
+                (0.00588347273456366, 1.065019447789682, 0.02261439342501856), 
+                (0.0056002259967936775, 1.0001733579751741, 0.05081497410394928), 
+                (0.005710840555584344, 0.7446334868201906, 0.03861705005172988)]
+
+
+def run_ga(generations=10, organisms_per_generation=10, cutoff=5, initial_pool=initial_pool_2, fitness=lambda o:o+1, max_=False):
     organisms = initial_pool
     for g in range(generations):
         """sort organisms by fitness and apply cutoff"""
-        organisms_ = sorted(organisms, key=lambda f:fitness(f)[0], reverse=max_)[0:min(cutoff, len(organisms) - 1)]
+        organisms_ = sorted(organisms, key=lambda f:fitness(f)[0], reverse=max_)[0:min(cutoff, len(organisms))]
 
         print "generation:%s organisms:\n%s\n\n" % (g, "\n".join([str((o, fitness(o)[0])) for o in organisms_]))
 
@@ -23,9 +30,6 @@ def run_ga(generations=10, organisms_per_generation=10, cutoff=5, initial_pool=i
             #new_org = (rnd*org1[0] + (1-rnd)*org2[0], rnd*org1[1] + (1-rnd)*org2[1], rnd*org1[2] + (1-rnd)*org2[2])
             new_org = (abs(random.uniform(-1, 1)*0.1+org1[0]), abs(random.uniform(-1, 1)*0.1+org1[1]), abs(random.uniform(-1, 1)*0.1+org1[2]))
             organisms_.append(new_org)
-            print "generated organism:{}".format(new_org)
-
-        print "finished generating organisms\n\n"
 
         organisms = organisms_
 
