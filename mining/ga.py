dc9238a3e1030956bc181aff49ea910f7276479a
Yonatan Oren <yonatano@gmail.com>
2015-07-06 01:25:25 -0700

    ga tests multiple products

--- mining/ga.py
+++ mining/ga.py
@@ -7,31 +7,40 @@ initial_pool_ = [(0.05, 0.1, 0.1),
                 (0.048, 0.2, 1.0),
                 (0.049, 1.0, 0.3)]
 
-"best constants after 20 generations for n0*pmi + n1*frequency"
-best_addition = [(0.005956095865679523, 0.8221104204538621, 0.007352881885527851), 
-                (0.006145872038018127, 0.9009544044452957, 0.00858503010435533), 
-                (0.006054769683361744, 1.1708753178730655, 0.02111922487975089), 
-                (0.006088804122235905, 0.9196975120606208, 0.009456202138401908), 
-                (0.005642846491121206, 0.9725525244043128, 0.016934542993977213)]
+"best constants after ~60 generations for n0*pmi + n1*frequency"
+best_addition = [(0.0183513596938029, 0.3135673133977043, 0.004890210734243838),
+                (0.2528731352604893, 0.9699263089994359, 0.013003233775384704),
+                (0.2788266998623834, 0.9804099106296693, 0.012776263015842337),
+                (0.381348689822024, 1.0953649343654606, 0.012729834994913869),
+                (0.3886202489674002, 1.0344276839102837, 0.012812949829223763)]
+
+"""
+great with agents_of_shield && amazon_fire
+((0.0183513596938029, 0.3135673133977043, 0.004890210734243838), 2)
+((0.2528731352604893, 0.9699263089994359, 0.013003233775384704), 2)
+((0.2788266998623834, 0.9804099106296693, 0.012776263015842337), 2)
+((0.381348689822024, 1.0953649343654606, 0.012729834994913869), 2)
+((0.3886202489674002, 1.0344276839102837, 0.012812949829223763), 2)
+"""
 
 def run_ga(generations=10, organisms_per_generation=10, cutoff=5, initial_pool=best_addition, fitness=lambda o:o+1, max_=False):
     organisms = initial_pool
     for g in range(generations):
         """sort organisms by fitness and apply cutoff"""
-        organisms_ = sorted(organisms, key=lambda f:fitness(f)[0], reverse=max_)[0:min(cutoff, len(organisms))]
+        organisms_ = sorted(organisms, key=fitness, reverse=max_)[0:min(cutoff, len(organisms))]
 
-        print "generation:%s organisms:\n%s\n\n" % (g, "\n".join([str((o, fitness(o)[0])) for o in organisms_]))
+        print "generation:%s organisms:\n%s\n\n" % (g, "\n".join([str((o, fitness(o))) for o in organisms_]))
 
         """fill pool with random organisms generated from top orgs"""
         for index in range(organisms_per_generation - len(organisms_)):
             """pick two random organisms and combine their traits randomly"""
             org1 = organisms_[int(random.random() * (len(organisms_) - 1))]
-            new_org = (abs(random.uniform(-1, 1)+org1[0]), abs(random.uniform(-1, 1)+org1[1]), abs(random.uniform(-1, 1)+org1[2]))
+            new_org = (abs(random.uniform(-1, 1)*0.1+org1[0]), abs(random.uniform(-1, 1)*0.1+org1[1]), abs(random.uniform(-1, 1)+org1[2]))
             organisms_.append(new_org)
 
         organisms = organisms_
 
-    print "top organisms:\n%s" % sorted(organisms, key=lambda f:fitness(f)[0], reverse=max_)[0:cutoff]
+    print "top organisms:\n%s" % sorted(organisms, key=fitness, reverse=max_)[0:cutoff]
 
 
 #run_ga(organisms_per_generation=100, cutoff=5, fitness=pmi_freq_fitness)
--- mining/mine_reviews.py
+++ mining/mine_reviews.py
@@ -9,14 +9,14 @@ from nltk.tag import pos_tag
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 trigram_measures = nltk.collocations.TrigramAssocMeasures()
 
-def load_review_texts():
-    if os.path.exists("reviews_texts_words.pickle"):
-        data = open("reviews_texts_words.pickle", "r")
+def load_review_texts(file_name):
+    if os.path.exists("%s.pickle"%file_name):
+        data = open("%s.pickle"%file_name, "r")
         reviews_texts_words = pickle.loads(data.read())
         data.close()
         return reviews_texts_words
     else:
-        data = open("../crawler/reviews.json", "r")
+        data = open("../crawler/%s.json"%file_name, "r")
         json_ = json.loads(data.read())
 
         reviews_texts = "\n".join([r['reviewText'] for r in json_])
@@ -34,14 +34,16 @@ def load_review_texts():
         reviews_texts_words = [w for w in reviews_texts_words if w[0] not in stop]
 
         print "saving..."
-        save = open("reviews_texts_words.pickle", "w")
+        save = open("%s.pickle"%file_name, "w")
         save.write(pickle.dumps(reviews_texts_words))
         save.close()
         return reviews_texts_words
 
-def save_bigrams_data():
+def save_bigrams_data(file_name):
     """find bigram collocations"""
-    finder = BigramCollocationFinder.from_words(load_review_texts())
+    raw_data = load_review_texts(file_name)
+    raw_data = [(r[0].lower(), r[1]) for r in raw_data]
+    finder = BigramCollocationFinder.from_words(raw_data)
 
     """apply custom filter onto finder"""
     # finder.apply_freq_filter(int(1.0/100.0*6894.0))
@@ -50,16 +52,16 @@ def save_bigrams_data():
     pmi_scores = finder.score_ngrams(bigram_measures.pmi)
     frequencies = dict(finder.ngram_fd.items())
 
-    f = open('bigram_scores.json', 'w')
+    f = open('%s.json'%file_name, 'w')
     data = [{'w1':t[0][0], 'w2':t[0][1], 'pmi':t[1], 'fr':frequencies[(t[0][0], t[0][1])]} for t in pmi_scores]
     f.write(simplejson.dumps(sorted(data, key=lambda x:x['fr']), indent=4))
     f.close()
 
 """trim results lower than n frequency and sort by pmi"""
-def top_results(min_freq=1.0, num_res=14, allowed_pos=['NN'], sort_f=lambda x:x['pmi']):
-    f = open('bigram_scores.json', 'r')
+def top_results(file_name, min_freq=1.0, num_res=14, allowed_pos=['NN'], sort_f=lambda x:x['pmi']):
+    f = open("%s.json"%file_name, 'r')
     data = json.loads(f.read())
-    min_freq = int(float(min_freq)/100*len(data))
+    #min_freq = int(float(min_freq)/100*len(data))
     data = [d for d in data if int(d['fr']) > min_freq]
     data = [d for d in data if d['w1'][1] in allowed_pos and d['w2'][1] in allowed_pos]
     data = sorted(data, key=sort_f, reverse=True)
@@ -67,28 +69,46 @@ def top_results(min_freq=1.0, num_res=14, allowed_pos=['NN'], sort_f=lambda x:x[
     return data[0:min(num_res, len(data)-1)]
 
 def pmi_freq_fitness(organism):
-    #print "calculating fitness for: {}".format(organism)
+    files = ['amazon_fire', 'agents_of_shield', 'galaxys5']
+
+    best = {'amazon_fire':['battery life',
+                        'back button',
+                        'sim card',
+                        'screen protector',
+                        'prime membership',
+                        'pixel density',
+                        'image stabilization',
+                        'status bar',
+                        'learning curve',
+                        'gorilla glass',
+                        'voice recognition',
+                        'notification light',
+                        'operating system',
+                        'instant video'],
+            'agents_of_shield':['edge seat',
+                        'character development',
+                        'story line',
+                        'tv show'],
+            'galaxys5':['finger print',
+                        'fingerprint scanner',
+                        'heart rate',
+                        'rate monitor',
+                        'sim card',
+                        'water resistant',
+                        'water resistance'
+                        'charging port',
+                        'home button',
+                        'operating system',
+                        'battery life']
+        }
+
     """calculate fitness for an organism, an organism is (n0, n1, n2)"""
-    results = ["%s %s" % (r['w1'][0], r['w2'][0]) for r in top_results(min_freq=organism[0], 
-                                                                       sort_f=lambda x:float(organism[1])*x['pmi']+float(organism[2])*x['fr'])]
+    results = {f:["%s %s" % (r['w1'][0], r['w2'][0]) for r in top_results(file_name=f, min_freq=5, num_res=len(best[f]), 
+                                                                       sort_f=lambda x:float(organism[1])*x['pmi']+float(organism[2])*x['fr'])] for f in files}
 
-    best = ['battery life',
- 'back button',
- 'sim card',
- 'screen protector',
- 'prime membership',
- 'pixel density',
- 'image stabilization',
- 'status bar',
- 'learning curve',
- 'gorilla glass',
- 'voice recognition',
- 'notification light',
- 'operating system',
- 'instant video']
-
-    diff = list(set(best) - set(results))
-    return (len(diff), results)
+    diffs = [list(set(best[f]) - set(results[f])) for f in files]
+    
+    return sum([len(d) for d in diffs])
 
 """use genetic algorithm to optimize values of min_freq, and constants for pmi and fr"""
 # from ga import run_ga
