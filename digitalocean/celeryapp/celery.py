38f108bd80b754ec182a056fc11b036b87bd9ac7
Yonatan Oren <yonatano@gmail.com>
2015-08-13 16:03:39 -0700

    stuff

--- digitalocean/celeryapp/celery.py
+++ digitalocean/celeryapp/celery.py
@@ -4,13 +4,13 @@ from celery import Celery
 
 app = Celery('celeryapp',
              broker='amqp://yonatan:yonatan09@localhost:5672/yvhost',
-             backend='amqp://',
+             backend='rpc://',
              include=['celeryapp.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     BROKER_URL = 'amqp://yonatan:yonatan09@localhost:5672/yvhost',
-    CELERY_RESULT_BACKEND = 'ampq://',
+    CELERY_RESULT_BACKEND = 'rpc://',
     CELERY_TASK_SERIALIZER = 'json',
     CELERY_RESULT_SERIALIZER = 'json',
     CELERY_ACCEPT_CONTENT=['json'],
