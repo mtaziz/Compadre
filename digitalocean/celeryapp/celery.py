0e05c0886818387974c19b87e295bf3cfffa3a59
Yonatan Oren <yonatano@gmail.com>
2015-08-13 15:59:56 -0700

    conf stuff

--- digitalocean/celeryapp/celery.py
+++ digitalocean/celeryapp/celery.py
@@ -8,7 +8,15 @@ app = Celery('celeryapp',
              include=['celeryapp.tasks'])
 
 # Optional configuration, see the application user guide.
-app.config_from_object('celeryapp.celeryconfig')
+app.conf.update(
+    BROKER_URL = 'amqp://yonatan:yonatan09@localhost:5672/yvhost',
+    CELERY_RESULT_BACKEND = 'ampq://',
+    CELERY_TASK_SERIALIZER = 'json',
+    CELERY_RESULT_SERIALIZER = 'json',
+    CELERY_ACCEPT_CONTENT=['json'],
+    CELERY_TIMEZONE = 'US/Pacific',
+    CELERY_ENABLE_UTC = True
+)
 
 if __name__ == '__main__':
     app.start()
