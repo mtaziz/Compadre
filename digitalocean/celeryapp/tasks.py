5c18257a0f1db7af2d9f1d62a369ecaabfb5bbcf
Yonatan Oren <yonatano@gmail.com>
2015-08-13 10:59:05 -0700

    logging to a file

--- digitalocean/celeryapp/tasks.py
+++ digitalocean/celeryapp/tasks.py
@@ -12,16 +12,23 @@ class ItemPipeline(object):
         #              attributes=item['attributes'],
         #              widgets=item['widgets'],
         #              category=spider.class_name)104.236.177.107
-        # item_.put()
+        # item_.put()eval "$(ssh-agent -s)"
 
 @app.task
 def scrape(class_name, spider_name, start_urls, widgets):
+    print "scrape executing with:{0}\n{1}\n{2}\n{3}".format(class_name, 
+                                                            spider_name, 
+                                                            start_urls, widgets)
+    file_ = open('log.txt', 'a')
+    file_.write("saving:{}".format(item['name']))
+    file_.close()
+
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
         'ITEM_PIPELINES': {'__main__.ItemPipeline': 1},
     })
 
-    logging.log(logging.INFO, "scrape celery task running")
+    print "about to call process"
     process.crawl(spider_name, start_urls=start_urls, 
                   item_template=ITEM_TEMPLATE, widgets=widgets, 
                   class_name=class_name)
