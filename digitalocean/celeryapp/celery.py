006db97fc49fabfedd675f8b5afa1a15e5a1923d
Yonatan Oren <yonatano@gmail.com>
2015-08-13 16:05:57 -0700

    test stuf

--- digitalocean/celeryapp/celery.py
+++ digitalocean/celeryapp/celery.py
@@ -3,14 +3,12 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('celeryapp',
-             broker='amqp://yonatan:yonatan09@localhost:5672/yvhost',
-             backend='rpc://',
              include=['celeryapp.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     BROKER_URL = 'amqp://yonatan:yonatan09@localhost:5672/yvhost',
-    CELERY_RESULT_BACKEND = 'rpc://',
+    CELERY_RESULT_BACKEND = 'ampq',
     CELERY_TASK_SERIALIZER = 'json',
     CELERY_RESULT_SERIALIZER = 'json',
     CELERY_ACCEPT_CONTENT=['json'],
