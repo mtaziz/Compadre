337b0e9dc16a5550be96c388408fb3526ec76e45
Yonatan Oren <yonatano@gmail.com>
2015-08-13 11:41:38 -0700

    changed user/vhost on celery

--- digitalocean/celeryapp/celery.py
+++ digitalocean/celeryapp/celery.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('celeryapp',
-             broker='amqp://',
+             broker='amqp://yonatan:yonatan09@localhost:5672/yvhost',
              backend='amqp://',
              include=['celeryapp.tasks'])
 
